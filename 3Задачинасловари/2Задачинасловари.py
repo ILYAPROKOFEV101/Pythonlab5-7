ba2ce551af2c7c1d727a31fda6b91aa3dfd06792
Ilya Prokofev <87380526+ilyaprofev@users.noreply.github.com>
2024-10-03 12:17:43 +0300

    old dane

--- 3Задачинасловари/2Задачинасловари.py
+++ 3Задачинасловари/2Задачинасловари.py
@@ -1,13 +1,34 @@
-bank = \
-    {
-    5289: 'карта',
-    2535: 'ноутбук',
-    2432: 'картина',
-    3434: 'лес',
-    1488: 'гойда'
-    }
+import hashlib
 
-token = input('Введите ')
-getdata = bank['1488']
-print('getdata')
+# Словарь с банковскими ячейками и кодовыми словами
+bank_cells = {
+    101: "карта",
+    102: "лес",
+    103: "дом",
+    104: "ноутбук",
+    105: "стол"
+}
+
+
+# Функция для поиска ячейки по кодовому слову
+def find_bank_cell_by_codeword(codeword):
+    # Поиск номера ячейки по кодовому слову
+    for cell_number, word in bank_cells.items():
+        if word == codeword:
+            # Хеширование кодового слова с помощью MD5
+            md5_hash = hashlib.md5(codeword.encode()).hexdigest()
+            return cell_number, md5_hash
+
+    return None, None  # Если кодовое слово не найдено
+
+
+# Пример использования
+codeword_input = input("Введите кодовое слово: ")
+cell_number, md5_hash = find_bank_cell_by_codeword(codeword_input)
+
+if cell_number:
+    print(f"Номер ячейки: {cell_number}")
+    print(f"MD5-хеш кодового слова: {md5_hash}")
+else:
+    print("Кодовое слово не найдено.")
 
